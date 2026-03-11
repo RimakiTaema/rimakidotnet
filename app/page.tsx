76a77db212104d768f27a68feebe7c6e56aef7e1
RimakiTaema <txwfyxa@gmail.com>
2026-03-12 00:13:39 +0700

    why i fix these again

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,37 @@ import {
   AlertDialogTitle,
   AlertDialogDescription,
 } from "@/components/ui/alert-dialog";
+import {SidebarSeparator} from "@/components/ui/sidebar";
+import {Card, CardDescription, CardTitle} from "@/components/ui/card";
 
 export default function Home() {
+    const card = [
+        {title: "Placeholder 1", description: "Placeholder 1"},
+        {title: "Placeholder 2", description: "Placeholder 2"},
+        {title: "Placeholder 3", description: "Placeholder 3"},
+        {title: "Placeholder 4", description: "Placeholder 4"},
+        {title: "Placeholder 5", description: "Placeholder 5"},
+    ]
   return (
-    <div className="flex min-h-screen h-32 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <p>Background content here</p>
-
+    <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black p-6">
+      <div className="text-2xl font-semibold">
+        Projects
+      </div>
+      <SidebarSeparator />
+        <div className="mt-4">
+            <div className="flex flex-row gap-3 overflow-x-auto">
+                {card.map((card, index) => (
+                    <Card key={index} className="w-64 h-64 shrink-0">
+                        <CardTitle className="text-2xl font-semibold text-gray-900 px-2">
+                            {card.title}
+                        </CardTitle>
+                        <CardDescription className="text-lg text-gray-900 px-2">
+                            {card.description}
+                        </CardDescription>
+                    </Card>
+                ))}
+            </div>
+        </div>
       <AlertDialog open>
         <AlertDialogContent>
           <AlertDialogHeader>

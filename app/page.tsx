8b90ef662608888adef45805bc17a2423121a2a6
RimakiTaema <txwfyxa@gmail.com>
2026-03-12 00:26:20 +0700

    small changes

--- app/page.tsx
+++ app/page.tsx
@@ -18,15 +18,40 @@ export default function Home() {
         {title: "Placeholder 4", description: "Placeholder 4"},
         {title: "Placeholder 5", description: "Placeholder 5"},
     ]
+
+    const cagtcard = [
+        {title: "Placeholder 1", description: "Placeholder 1"},
+        {title: "Placeholder 2", description: "Placeholder 2"},
+        {title: "Placeholder 3", description: "Placeholder 3"},
+    ]
+
   return (
     <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black p-8">
       <div className="text-2xl font-semibold">
         Projects
       </div>
       <SidebarSeparator />
-        <div className="mt-4">
+      <div className="mt-4">
             <div className="flex flex-row gap-3 overflow-x-auto">
                 {card.map((card, index) => (
+                    <Card key={index} className="w-64 h-64 shrink-0">
+                        <CardTitle className="text-2xl font-semibold text-gray-900 px-2">
+                            {card.title}
+                        </CardTitle>
+                        <CardDescription className="text-lg text-gray-900 px-2">
+                            {card.description}
+                        </CardDescription>
+                    </Card>
+                ))}
+            </div>
+      </div>
+        <div className="text-2xl font-semibold">
+            Categorises
+        </div>
+        <SidebarSeparator />
+        <div className="mt-4">
+            <div className="flex flex-row gap-3 overflow-x-auto">
+                {cagtcard.map((card, index) => (
                     <Card key={index} className="w-64 h-64 shrink-0">
                         <CardTitle className="text-2xl font-semibold text-gray-900 px-2">
                             {card.title}
